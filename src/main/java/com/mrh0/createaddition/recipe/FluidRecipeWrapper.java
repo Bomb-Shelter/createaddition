2adf7bbfb9ae0f45c4a335f89d323f68259bf18c
Naz <bluspringindohq@gmail.com>
2025-08-09 21:07:14 +0800

    Fix obj rendering and remapJar failing

--- src/main/java/com/mrh0/createaddition/recipe/FluidRecipeWrapper.java
+++ src/main/java/com/mrh0/createaddition/recipe/FluidRecipeWrapper.java
@@ -8,22 +8,12 @@ import net.minecraft.world.item.Items;
 import net.minecraft.world.item.crafting.RecipeInput;
 import org.jetbrains.annotations.NotNull;
 
-public class FluidRecipeWrapper implements Container, RecipeInput {
+public class FluidRecipeWrapper implements RecipeInput {
 
 	public FluidStack fluid;
 	
 	public FluidRecipeWrapper(FluidStack fluid) {
 		this.fluid = fluid;
-	}
-	
-	@Override
-	public void clearContent() {
-		//fluid = new FluidStack(Fluids.EMPTY.getFluid(), 0);
-	}
-
-	@Override
-	public int getContainerSize() {
-		return 0;
 	}
 
 	@Override
@@ -39,29 +29,6 @@ public class FluidRecipeWrapper implements Container, RecipeInput {
 	@Override
 	public int size() {
 		return 0;
-	}
-
-	@Override
-	public @NotNull ItemStack removeItem(int p_70298_1_, int p_70298_2_) {
-		return new ItemStack(Items.AIR);
-	}
-
-	@Override
-	public @NotNull ItemStack removeItemNoUpdate(int p_70304_1_) {
-		return new ItemStack(Items.AIR);
-	}
-
-	@Override
-	public void setItem(int p_70299_1_, @NotNull ItemStack stack) {
-	}
-
-	@Override
-	public void setChanged() {
-	}
-
-	@Override
-	public boolean stillValid(@NotNull Player player) {
-		return true;
 	}
 
 }
