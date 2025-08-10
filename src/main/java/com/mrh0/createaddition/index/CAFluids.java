46c802d7ca5cc279b8375c53a971d7e44d9f76c2
Naz <bluspringindohq@gmail.com>
2025-08-10 09:24:53 +0800

    Try to fix fluid rendering

--- src/main/java/com/mrh0/createaddition/index/CAFluids.java
+++ src/main/java/com/mrh0/createaddition/index/CAFluids.java
@@ -3,13 +3,24 @@ package com.mrh0.createaddition.index;
 import com.mrh0.createaddition.CreateAddition;
 import com.simibubi.create.AllFluids;
 
+import com.simibubi.create.infrastructure.fabric.client.CustomRenderHandlerFluidType;
 import com.tterrag.registrate.util.entry.FluidEntry;
+import io.github.fabricators_of_create.porting_lib.event.client.FogEvents;
 import io.github.fabricators_of_create.porting_lib.fluids.BaseFlowingFluid;
 import io.github.fabricators_of_create.porting_lib.fluids.FluidStack;
+import net.createmod.catnip.platform.CatnipServices;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.fabricmc.fabric.api.client.render.fluid.v1.FluidRenderHandlerRegistry;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.core.BlockPos;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.BlockAndTintGetter;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.material.Fluid;
 import net.minecraft.world.level.material.FluidState;
+import org.joml.Vector3f;
 
 public class CAFluids {
 	static {
@@ -73,5 +84,46 @@ public class CAFluids {
 			.properties(p -> p.stacksTo(1))
 			.register();
 		BIOETHANOL = bioethanol.register();
+
+		registerClient();
+	}
+
+	public static void registerClient() {
+		CatnipServices.PLATFORM.executeOnClientOnly(() -> () -> {
+			ClientFluidEvents.registerFluidRenderHandler(SEED_OIL);
+			ClientFluidEvents.registerFluidRenderHandler(BIOETHANOL);
+		});
+	}
+
+	@Environment(EnvType.CLIENT)
+	private static class ClientFluidEvents {
+		public static <T extends BaseFlowingFluid> void registerFluidRenderHandler(FluidEntry<T> fluid) {
+			if (fluid.getType() instanceof CustomRenderHandlerFluidType fluidType) {
+				FluidRenderHandlerRegistry.INSTANCE.register(fluid.getSource(), fluid.get(), fluidType.getRenderHandler());
+			}
+
+			if (fluid.getType() instanceof AllFluids.TintedFluidType tintedFluidType) {
+				FogEvents.SET_COLOR.register((data, partialTicks) -> {
+					Level level = data.getCamera().getEntity().level();
+					FluidState fluidState = level.getFluidState(data.getCamera().getBlockPosition());
+					if (fluidState.is((Fluid) fluid.getSource()) || fluidState.is(fluid.get())) {
+						Vector3f modified = tintedFluidType.modifyFogColor(data.getCamera(), partialTicks, (ClientLevel) level, Minecraft.getInstance().options.getEffectiveRenderDistance(), Minecraft.getInstance().gameRenderer.getDarkenWorldAmount(partialTicks), new Vector3f(data.getRed(), data.getGreen(), data.getBlue()));
+						data.setRed(modified.x);
+						data.setGreen(modified.y);
+						data.setBlue(modified.z);
+					}
+				});
+
+				FogEvents.RENDER_FOG.register((mode, type, camera, partialTick, renderDistance, nearDistance, farDistance, shape, fogData) -> {
+					Level level = camera.getEntity().level();
+					FluidState fluidState = level.getFluidState(camera.getBlockPosition());
+					if (fluidState.is((Fluid) fluid.getSource()) || fluidState.is(fluid.get())) {
+						tintedFluidType.modifyFogRender(camera, mode, renderDistance, partialTick, nearDistance, farDistance, shape);
+					}
+
+					return false;
+				});
+			}
+		}
 	}
 }
